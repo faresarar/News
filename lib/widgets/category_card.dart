1e0d9b8917a98f74d48694181a739539584ce15d
faresarar <faresarar455@gmail.com>
2024-07-19 18:20:22 +0100

    Adding category_card.dart

---
+++ lib/widgets/category_card.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class CategoryCard extends StatelessWidget {
+  const CategoryCard({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 100,
+      width: 200,
+      decoration: BoxDecoration(
+          image: const DecorationImage(
+            image: AssetImage("assets/sports.avif"),
+            fit: BoxFit.fill,
+          ),
+          borderRadius: BorderRadius.circular(16)),
+      child: const Center(
+        child: Text(
+          "Sports",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+}
