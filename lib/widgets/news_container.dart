9aa7bb8a992fe0f821744313f67ede13e9fce48f
faresarar <faresarar455@gmail.com>
2024-07-19 17:48:36 +0100

    Setup Project

---
+++ lib/widgets/news_container.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NewsContainer extends StatelessWidget {
+  const NewsContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold();
+  }
+}
