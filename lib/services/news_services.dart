8d2e162c11c5fd1b3a1b19a627331a279fd0b424
faresarar <faresarar455@gmail.com.com>
2024-07-31 18:12:36 +0100

    Adding article_model.dart

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -9,9 +9,5 @@ class NewsService {
         "https://newsapi.org/v2/top-headlines?country=us&apiKey=8002fc5d48754478a73b438b21adf85a&country=dz&category=general");
     Map<String, dynamic> jsonData = response.data;
     List<dynamic> articles = jsonData['articles'];
-    print(articles);
-    for (var article in articles) {
-      print(article['author']);
-    }
   }
 }
