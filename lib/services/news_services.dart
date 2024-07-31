adfcb72301cc3b2f44f1ae1acde46af790e17c58
faresarar <faresarar455@gmail.com.com>
2024-07-31 17:40:47 +0100

    Requesting general news

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -1,7 +1,11 @@
 import 'package:dio/dio.dart';
+
 class NewsService {
   final Dio dio;
 
   NewsService(this.dio);
-  getNews(){}
+  getNews() async {
+    Response response = await dio.get(
+        "https://newsapi.org/v2/top-headlines?country=us&apiKey=8002fc5d48754478a73b438b21adf85a&country=dz&category=general");
+  }
 }
