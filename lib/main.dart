0dfa844a03c40d184d155ed0fb053a39cb09e614
faresarar <faresarar455@gmail.com.com>
2024-07-31 18:06:27 +0100

    Response

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:news_app/services/news_services.dart';
 
 import 'screens/home_screen.dart';
 
 void main() {
+  NewsService(Dio()).getNews();
   runApp(const MyApp());
 }
 
--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -7,5 +7,11 @@ class NewsService {
   getNews() async {
     Response response = await dio.get(
         "https://newsapi.org/v2/top-headlines?country=us&apiKey=8002fc5d48754478a73b438b21adf85a&country=dz&category=general");
+    Map<String, dynamic> jsonData = response.data;
+    List<dynamic> articles = jsonData['articles'];
+    print(articles);
+    for (var article in articles) {
+      print(article['author']);
+    }
   }
 }
