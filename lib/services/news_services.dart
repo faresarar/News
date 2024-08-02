73917a3079f1caddd9d345fb9e55be275476445a
faresarar <faresarar455@gmail.com>
2024-08-02 17:26:04 +0100

    finish finish getNews method

--- lib/services/news_services.dart
+++ lib/services/news_services.dart
@@ -1,13 +1,25 @@
 import 'package:dio/dio.dart';
 
+import '../models/article_model.dart';
+
 class NewsService {
   final Dio dio;
 
   NewsService(this.dio);
-  getNews() async {
+  Future<List<ArticleModel>> getNews() async {
     Response response = await dio.get(
         "https://newsapi.org/v2/top-headlines?country=us&apiKey=8002fc5d48754478a73b438b21adf85a&country=dz&category=general");
     Map<String, dynamic> jsonData = response.data;
     List<dynamic> articles = jsonData['articles'];
+    List<ArticleModel> articlesList = [];
+    for (var article in articles) {
+      ArticleModel articleModel = ArticleModel(
+        image: article['urlToImage'],
+        title: article['title'],
+        subTitle: article['description'],
+      );
+      articlesList.add(articleModel);
+    }
+    return articlesList;
   }
 }
