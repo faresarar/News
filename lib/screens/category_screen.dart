7d843ff9ba01e768104902514c94d2964f366cb9
faresarar <faresarar455@gmail.com>
2024-08-03 23:33:10 +0100

    Displaying data in category_screen.dart

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,12 +1,18 @@
 import 'package:flutter/material.dart';
 
+import '../widgets/news_list_view_builder.dart';
+
 class CategoryScreen extends StatelessWidget {
   const CategoryScreen( {super.key});
   //final String category;
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: SizedBox(),
+      body: CustomScrollView(
+        slivers: [
+          NewsListViewBuilder()
+        ],
+      ),
     );
   }
 }
