42375c09a37c012dca532a53d885155908a2c91a
faresarar <faresarar455@gmail.com>
2024-08-03 23:17:30 +0100

    adding category_screen.dart

--- lib/screens/category_screen.dart
+++ lib/screens/category_screen.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
 
 class CategoryScreen extends StatelessWidget {
-  const CategoryScreen( {super.key , required this.category});
-  final String category;
+  const CategoryScreen( {super.key});
+  //final String category;
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: CustomScrollView(),
+    return const Scaffold(
+      body: SizedBox(),
     );
   }
 }
--- lib/widgets/category_card.dart
+++ lib/widgets/category_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:news_app/screens/category_screen.dart';
 
 import '../models/category_model.dart';
 
@@ -7,24 +8,35 @@ class CategoryCard extends StatelessWidget {
   final CategoryModel categoryModel;
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(right: 16),
-      child: Container(
-        height: 100,
-        width: 200,
-        decoration: BoxDecoration(
-            image: DecorationImage(
-              image: AssetImage(categoryModel.image),
-              fit: BoxFit.fill,
-            ),
-            borderRadius: BorderRadius.circular(16)),
-        child: Center(
-          child: Text(
-            categoryModel.categoryName,
-            style: const TextStyle(
-              color: Colors.white,
-              fontSize: 18,
-              fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () {
+        Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (context) {
+              return const CategoryScreen();
+            },
+          ),
+        );
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(right: 16),
+        child: Container(
+          height: 100,
+          width: 200,
+          decoration: BoxDecoration(
+              image: DecorationImage(
+                image: AssetImage(categoryModel.image),
+                fit: BoxFit.fill,
+              ),
+              borderRadius: BorderRadius.circular(16)),
+          child: Center(
+            child: Text(
+              categoryModel.categoryName,
+              style: const TextStyle(
+                color: Colors.white,
+                fontSize: 18,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
         ),
